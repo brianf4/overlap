802de9798f765296bc9f956ada8345d63e4f66f5
Brian Fuentes <Brian.Fuentes04@gmail.com>
2026-06-27 15:54:10 -0700

    initial commit

---
+++ frontend/src/components/Calendar.jsx
@@ -0,0 +1,17 @@
+function Calendar() {
+    return (
+        <div className="calendar-containter">
+            <div className="week">
+                <div>Sun</div>
+                <div>Mon</div>
+                <div>Tue</div>
+                <div>Wed</div>
+                <div>Thu</div>
+                <div>Fri</div>
+                <div>Sat</div>
+            </div>
+        </div>
+    );
+}
+
+export default Calendar;
