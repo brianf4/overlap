81fb3060a2bd7955a053168ef1f17fa75f125f7e
Brian Fuentes <Brian.Fuentes04@gmail.com>
2026-06-27 16:08:15 -0700

    currently trying to make calendar

--- frontend/src/components/Calendar.jsx
+++ frontend/src/components/Calendar.jsx
@@ -1,14 +1,16 @@
 function Calendar() {
     return (
         <div className="calendar-containter">
-            <div className="week">
-                <div>Sun</div>
-                <div>Mon</div>
-                <div>Tue</div>
-                <div>Wed</div>
-                <div>Thu</div>
-                <div>Fri</div>
-                <div>Sat</div>
+            <div className="calendar">
+                <div className="week">
+                    <div>Sun</div>
+                    <div>Mon</div>
+                    <div>Tue</div>
+                    <div>Wed</div>
+                    <div>Thu</div>
+                    <div>Fri</div>
+                    <div>Sat</div>
+                </div>
             </div>
         </div>
     );
