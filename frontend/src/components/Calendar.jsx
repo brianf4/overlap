f972271b32e8922e7ae577326a795b45b8d9c645
Brian Fuentes <Brian.Fuentes04@gmail.com>
2026-06-29 00:50:31 -0700

    finally aligned all columns and rows in calendar

--- frontend/src/components/Calendar.jsx
+++ frontend/src/components/Calendar.jsx
@@ -5,70 +5,66 @@ function Calendar() {
     }
     return (
         <div className="calendar-containter">
-            <div className="calendar">
-                <div className="week row1">
-                    <div className="corner">
-                        <span>test</span>
-                    </div>
-                    <div>
-                        <span>Sun</span>
-                    </div>
-                    <div>
-                        <span>Mon</span>
-                    </div>
-                    <div>
-                        <span>Tue</span>
-                    </div>
-                    <div>
-                        <span>Wed</span>
-                    </div>
-                    <div>
-                        <span>Thu</span>
-                    </div>
-                    <div>
-                        <span>Fri</span>
-                    </div>
-                    <div>
-                        <span>Sat</span>
-                    </div>
+            <div className="row1">
+                <div className="corner"></div>
+                <div>
+                    <span>Sun</span>
                 </div>
-                <div className="row2">
-                    <div className="corner"></div>
-                    <div className="column">
-                        {arr.map((_, i) => {
-                            return <div key={i + 1}></div>;
-                        })}
-                    </div>
-                    <div className="column">
-                        {arr.map((_, i) => {
-                            return <div key={i + 1}></div>;
-                        })}
-                    </div>
-                    <div className="column">
-                        {arr.map((_, i) => {
-                            return <div key={i + 1}></div>;
-                        })}
-                    </div>
-                    <div className="column">
-                        {arr.map((_, i) => {
-                            return <div key={i + 1}></div>;
-                        })}
-                    </div>
-                    <div className="column">
-                        {arr.map((_, i) => {
-                            return <div key={i + 1}></div>;
-                        })}
-                    </div>
-                    <div className="column">
-                        {arr.map((_, i) => {
-                            return <div key={i + 1}></div>;
-                        })}
-                    </div>
-                    <div className="column">
-                        {arr.map((_, i) => {
-                            return <div key={i + 1}></div>;
-                        })}
-                    </div>
+                <div>
+                    <span>Mon</span>
+                </div>
+                <div>
+                    <span>Tue</span>
+                </div>
+                <div>
+                    <span>Wed</span>
+                </div>
+                <div>
+                    <span>Thu</span>
+                </div>
+                <div>
+                    <span>Fri</span>
+                </div>
+                <div>
+                    <span>Sat</span>
+                </div>
+            </div>
+            <div className="row2">
+                <div className="corner"></div>
+                <div className="column">
+                    {arr.map((_, i) => {
+                        return <div key={i + 1}></div>;
+                    })}
+                </div>
+                <div className="column">
+                    {arr.map((_, i) => {
+                        return <div key={i + 1}></div>;
+                    })}
+                </div>
+                <div className="column">
+                    {arr.map((_, i) => {
+                        return <div key={i + 1}></div>;
+                    })}
+                </div>
+                <div className="column">
+                    {arr.map((_, i) => {
+                        return <div key={i + 1}></div>;
+                    })}
+                </div>
+                <div className="column">
+                    {arr.map((_, i) => {
+                        return <div key={i + 1}></div>;
+                    })}
+                </div>
+                <div className="column">
+                    {arr.map((_, i) => {
+                        return <div key={i + 1}></div>;
+                    })}
+                </div>
+                <div className="column">
+                    {arr.map((_, i) => {
+                        return <div key={i + 1}></div>;
+                    })}
                 </div>
             </div>
         </div>
