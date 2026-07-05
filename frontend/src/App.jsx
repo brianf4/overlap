ef04aa94ec7e8a159d4fe79c6ab042a2f9aa6a58
Brian Fuentes <Brian.Fuentes04@gmail.com>
2026-07-05 09:52:39 -0700

    added tailwind & daisyui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,30 @@ import Calendar from "./components/Calendar";
 function App() {
     return (
         <>
-            <div className="topBarCalendar"></div>
+            <div className="topBarCalendar">
+                <span className="text-green-500">Logo here</span>
+                {/* The button's popoverTarget attribute points to the modal's ID. ID must be unique for each modal */}
+                <button className="btn" popoverTarget="my-modal-2">
+                    Open
+                </button>
+
+                <div className="modal" id="my-modal-2" popover="true">
+                    <div className="modal-box">
+                        <h3 className="font-bold text-lg">Hello!</h3>
+                        <p className="py-4">
+                            Press ESC key or click the button below to close
+                        </p>
+                    </div>
+                    <div className="modal-backdrop">
+                        <button
+                            popoverTarget="my-modal-2"
+                            popoverTargetAction="hide"
+                        >
+                            close
+                        </button>
+                    </div>
+                </div>
+            </div>
             <Calendar></Calendar>
         </>
     );
