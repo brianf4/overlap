3a96f917b0a8f84abd36a3546a3117f077dc5c9a
Brian Fuentes <Brian.Fuentes04@gmail.com>
2026-08-16 22:33:41 -0700

    Calendar looks good. Was working on the height and alignment border issues. Its in a good area where I want.

--- frontend/src/components/Calendar.jsx
+++ frontend/src/components/Calendar.jsx
@@ -1,33 +1,21 @@
+const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+
 function Calendar() {
     let arr = [];
     for (let i = 1; i < 25; i++) {
         arr.push(i.toString());
     }
     return (
-        <div className="calendar-containter">
+        <>
+            {/* Row 1 */}
             <div className="row1">
                 <div className="corner"></div>
-                <div>
-                    <span>Sun</span>
-                </div>
-                <div>
-                    <span>Mon</span>
-                </div>
-                <div>
-                    <span>Tue</span>
-                </div>
-                <div>
-                    <span>Wed</span>
-                </div>
-                <div>
-                    <span>Thu</span>
-                </div>
-                <div>
-                    <span>Fri</span>
-                </div>
-                <div>
-                    <span>Sat</span>
-                </div>
+                {DAYS.map(day => (
+                    <div key={day}>
+                        <span>{day}</span>
+                    </div>
+                ))}
             </div>
             <div className="row2">
                 <div className="corner">
@@ -71,7 +59,7 @@ function Calendar() {
                     })}
                 </div>
             </div>
-        </div>
+        </>
     );
 }
 
--- frontend/src/pages/CalendarSheet.jsx
+++ frontend/src/pages/CalendarSheet.jsx
@@ -2,33 +2,33 @@ import Calendar from "../components/Calendar";
 
 function CalendarSheet() {
     return (
-        <>
-        <div className="topBarCalendar">
-            <span className="text-green-500">Logo here</span>
-            {/* The button's popoverTarget attribute points to the modal's ID. ID must be unique for each modal */}
-            <button className="btn" popoverTarget="my-modal-2">
-                Open
-            </button>
+        <div className="calendar-containter">
+            <div className="topBarCalendar">
+                <span className="text-green-500">Logo here</span>
+                {/* The button's popoverTarget attribute points to the modal's ID. ID must be unique for each modal */}
+                <button className="btn" popoverTarget="my-modal-2">
+                    Open
+                </button>
 
-            <div className="modal" id="my-modal-2" popover="true">
-                <div className="modal-box">
-                    <h3 className="font-bold text-lg">Hello!</h3>
-                    <p className="py-4">
-                        Press ESC key or click the button below to close
-                    </p>
-                </div>
-                <div className="modal-backdrop">
-                    <button
-                        popoverTarget="my-modal-2"
-                        popoverTargetAction="hide"
-                    >
-                        close
-                    </button>
+                <div className="modal" id="my-modal-2" popover="true">
+                    <div className="modal-box">
+                        <h3 className="font-bold text-lg">Hello!</h3>
+                        <p className="py-4">
+                            Press ESC key or click the button below to close
+                        </p>
+                    </div>
+                    <div className="modal-backdrop">
+                        <button
+                            popoverTarget="my-modal-2"
+                            popoverTargetAction="hide"
+                        >
+                            close
+                        </button>
+                    </div>
                 </div>
             </div>
+            <Calendar></Calendar>
         </div>
-        <Calendar></Calendar>
-        </>
     )
 }
 export default CalendarSheet;
